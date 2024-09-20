dccdc66c751f966585f1aa878cfbe7e681df6a76
Andremxsrs <ronnyandres120@gmail.com>
2024-09-20 09:37:40 -0500

    Traducción al español

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Configurar Filament Language Switch para cambiar entre inglés y español
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['es']);  // Aquí defines los idiomas que soporta tu panel
+        });
+
+        // Establecer la longitud de los strings en la base de datos
         Schema::defaultStringLength(191);
     }
 }
